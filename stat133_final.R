cf76dc41782f418aca7bacc0fb7f3ec0603621e0
tahabi <abizer@berkeley.edu>
2015-11-30 15:19:06 -0800

    departments

--- stat133_final.R
+++ stat133_final.R
@@ -4,20 +4,81 @@
 library(readr)
 library(ggplot2)
 library(dplyr)
+library(stringr)
 
 uctca_colnames <- c("Name", "Title", "Base", "Overtime", "Other", "Benefits", "Subtotal", "Total", "Year", "Notes", "Agency")
+
+# my working dir: ~/Dropbox/College/Freshman Year/Fall Semester/STAT 133/project
+setwd('~/Dropbox/College/Freshman Year/Fall Semester/STAT 133/project')
 
 uc2013 <- read_csv("university-of-california-2013.csv", col_names = uctca_colnames, skip = 1) %>% filter(Total > 1000)
 uc2014 <- read_csv("university-of-california-2014.csv", col_names = uctca_colnames, skip = 1) %>% filter(Total > 1000)
 
-employee_change = nrow(uc2014) - nrow(uc2013)
-new_employees = anti_join(uc2014, uc2013, by = 'Name')
+attributes(uc2013)$problems <- NULL # problems pollute the dataset
+attributes(uc2014)$problems <- NULL
 
-titles2013 <- uc2013 %>% select(Title) %>% distinct() 
-titles2014 <- uc2014 %>% select(Title) %>% distinct() 
+employee_change <- nrow(uc2014) - nrow(uc2013)
+#new_employees <- anti_join(uc2014, uc2013, by = 'Name')
 
-unique_titles <- merge(titles2013, titles2014)
+uc2013.names <- uc2013 %>% select(Name) %>% distinct() %>% mutate(Name = toupper(Name)) %>%  arrange(desc(Name))
+uc2014.names <- uc2014 %>% select(Name) %>% distinct() %>% arrange(desc(Name))
 
-names13 <- uc2013 %>% select(Name) %>% distinct()
-names14 <- uc2014 %>% select(Name) %>% distinct()
+cont_employees <- inner_join(uc2013.names, uc2014.names, by = 'Name')
+new_employees <- anti_join(uc2013.names, uc2014.names, by = 'Name')
 
+uc2013.titles <- uc2013 %>% select(Title) %>% distinct() 
+uc2014.titles <- uc2014 %>% select(Title) %>% distinct() 
+
+unique_titles <- merge(uc2013.titles, uc2014.titles)
+
+uc2013.total_overhead <- (uc2013 %>% summarize(total = sum(Total)))[[1, 1]]
+uc2014.total_overhead <- (uc2014 %>% summarize(total = sum(Total)))[[1, 1]]
+
+ml <- hash()
+ml[['Athletics']] <- '^ATH|COACH'
+ml[['Professor']] <- 'PROF'
+ml[['Administrator']] <- 'SUPV|MGR|ADMIN(ISTRATOR)?'
+ml[['Academic']] <- 'ACAD(EMIC)?'
+ml[['Maintenance']] <- 'MAINT|TECH'
+ml[['Accounting']] <- 'ACCOUNT(ANT|ING)'
+ml[['Assistant']] <- 'ASST'
+ml[['Associate']] <- 'ASSOC'
+ml[['Visiting']] <- 'VIS'
+ml[['Adjunct']] <- 'ADJ'
+ml[['Instructor']] <- 'INSTR'
+ml[['Nurse']] <- 'NURSE'
+ml[['Admissions']] <- 'ADMISSIONS'
+ml[['Engineer']] <- 'ENGR'
+ml[['Technician']] <- 'TCHN'
+ml[[]]
+
+get_dep <- function(title)
+{ # sapply matches regexes against the title, second line collects matched department names
+  out <- sapply(ml, function(pattern) grepl(pattern, title))
+  #out <- names(out)[out]
+  #ifelse(!is.null(out), out, 'Undetermined')
+}
+
+uc2013.employee_stats <- uc2013 %>% 
+  group_by(Title) %>% 
+    summarize(n = n(),
+              avg_base = mean(Base), 
+              avg_benefit = mean(Benefits),
+              avg_total = mean(Total),
+              group_total = sum(Total)) %>%
+  
+#uc2013.employee_stats <- uc2013.employee_stats %>%
+      #group_by(Title) %>%
+        #mutate(department = get_dep(Title))
+  
+departments <- sapply(ml, function(pat) grepl(pat, uc2013.employee_stats$Title))
+  
+
+uc2014.employee_stats <- uc2014 %>% 
+  group_by(Title) %>% 
+    summarize(n = n(), 
+              avg_base = mean(Base), 
+              avg_benefit = mean(Benefits),
+              avg_total = mean(Total),
+              group_total = sum(Total)) %>%
+      mutate(department = guess_department(Title))
