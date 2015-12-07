4f21c5c1f9e0bd2b194e5f4c6c32dcc02e209bf5
hjsutcliffe <hsutcliffe@berkeley.edu>
2015-12-06 17:08:05 -0800

    test

--- stat133_final.R
+++ stat133_final.R
@@ -10,7 +10,7 @@ library(hash)
 uctca_colnames <- c("Name", "Title", "Base", "Overtime", "Other", "Benefits", "Subtotal", "Total", "Year", "Notes", "Agency")
 
 # my working dir: ~/Dropbox/College/Freshman Year/Fall Semester/STAT 133/project
-setwd("C:/Users/Harry/stat133/final")
+setwd("C:/Users/Harry/stat133/final/project")
 
 uc2013 <- read_csv("university-of-california-2013.csv", col_names = uctca_colnames, skip = 1) %>% filter(Total > 1000)
 uc2014 <- read_csv("university-of-california-2014.csv", col_names = uctca_colnames, skip = 1) %>% filter(Total > 1000)
@@ -66,7 +66,7 @@ uc2013.employee_stats <- uc2013 %>%
             avg_base = mean(Base), 
             avg_benefit = mean(Benefits),
             avg_total = mean(Total),
-            group_total = sum(Total)) %>%
+            group_total = sum(Total))
   
   #uc2013.employee_stats <- uc2013.employee_stats %>%
   #group_by(Title) %>%
@@ -74,6 +74,53 @@ uc2013.employee_stats <- uc2013 %>%
   
   departments <- sapply(ml, function(pat) grepl(pat, uc2013.employee_stats$Title))
 
+uc2013.employee_stats.deps <- cbind(uc2013.employee_stats, departments)
+  
+uc2013.faculty <- uc2013.employee_stats.deps %>%
+  filter(Professor == TRUE | Instructor == TRUE) %>%
+  select(Title, n, avg_base, avg_benefit, avg_total, group_total, Professor, Instructor) %>%
+  mutate(category = 'Faculty')
+
+uc2013.faculty.professors <- uc2013.faculty %>% 
+  filter(Instructor == FALSE) %>%
+  select(-Professor, -Instructor) %>%
+  mutate(type = 'Professor')
+
+uc2013.faculty.instructors <- uc2013.faculty %>%
+  filter(Professor == FALSE) %>%
+  select(-Professor, -Instructor) %>%
+  mutate(type = 'Instructor')
+
+uc2013.faculty <- uc2013.faculty.professors %>%
+  bind_rows(uc2013.faculty.instructors)
+
+profvinst_plot <- ggplot(uc2013.faculty, aes(x = type, y = avg_base)) + geom_boxplot()
+
+uc2013.professors.adjunct <- uc2013.professors %>%
+  filter(Adjunct == TRUE)
+
+uc2013.professors.adjunct.only <- uc2013.professors.adjunct %>%
+  filter(Associate == FALSE & Assistant == FALSE)
+
+uc2013.professors.associate <- uc2013.professors %>% 
+  filter(Associate == TRUE)
+
+uc2013.professors.associate.only <- uc2013.professors.associate %>%
+  filter(Adjunct == FALSE & Assistant == FALSE)
+
+uc2013.professors.assistant <- uc2013.professors %>%
+  filter(Assistant == TRUE)
+
+uc2013.professors.assistant.only <- uc2013.professors.assistant %>%
+  filter(Adjunct == FALSE & Associate == FALSE)
+
+uc2013.professors.full <- uc2013.professors %>%
+  filter(Associate == FALSE & Adjunct == FALSE & Assistant == FALSE)
+
+filter_department <- function(df, dfdeps, departments) 
+{
+  df[which(dfdeps[, departments] == TRUE), ]
+}
 
 uc2014.employee_stats <- uc2014 %>% 
   group_by(Title) %>% 
@@ -83,3 +130,58 @@ uc2014.employee_stats <- uc2014 %>%
             avg_total = mean(Total),
             group_total = sum(Total)) %>%
   mutate(department = guess_department(Title))
+
+###############################################################
+#This section defines totals by department/category - Michael
+###############################################################
+
+#this function calculates the total in each department
+vec = c()
+sum_dep <- function(out){
+  for (i in 1:length(colnames(out))){
+    sum = sum(out[ , i])
+    vec = c(vec, sum)
+  }
+  names(vec) = colnames(out)
+  return(vec)
+}
+
+#using functions to get total in each department
+out = get_dep(uc2013$Title)
+tot_dep = sum_dep(out) #in a named list
+
+barplot(tot_dep, las = 2, main = 'Total in Each Defined Department, 2013')
+
+
+tot_dep2 = c(sum(tot_dep[c('Instructor', 'Lecturer',
+                           'Adjunct',
+                           'Academic', 'Visiting', 'Professor')]),
+             sum(tot_dep[c('Engineer', 'Technician', 'Accounting',
+                           'Nurse', 'Maintenance')]),
+             sum(tot_dep[c('Admissions', 'Administrator')]),
+             sum(tot_dep['Athletics']),
+             sum(tot_dep['Student']))
+names(tot_dep2) = c('Faculty', 'Staff', 'Admin', 'Athletics', 'Student')
+barplot(tot_dep2, main = 'Total by Category, 2013') #not a good representation....too much faculty
+
+#same thing for 2014
+
+out14 = get_dep(uc2014$Title)
+tot_dep14 = sum_dep(out14)
+barplot(tot_dep14, las = 2, main = 'Total in Each Defined Department, 2014')
+tot_dep14_2 = c(sum(tot_dep14[c('Instructor', 'Lecturer',
+                                'Adjunct',
+                                'Academic', 'Visiting', 'Professor')]),
+                sum(tot_dep14[c('Engineer', 'Technician', 'Accounting',
+                                'Nurse', 'Maintenance')]),
+                sum(tot_dep14[c('Admissions', 'Administrator')]),
+                sum(tot_dep14['Athletics']),
+                sum(tot_dep14['Student']))
+names(tot_dep14_2) = c('Faculty', 'Staff', 'Admin', 'Athletics', 'Student')
+barplot(tot_dep14_2, main = 'Total by Category, 2014')
+
+#graphs for both years extremely similiar
+
+#99,830 persons unaccounted for, or ~41%
+
+######################################################
