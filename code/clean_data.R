03d4ad4d43f038f177b1e52260223dd9557e527c
tahabi <abizer@berkeley.edu>
2015-12-15 21:40:34 -0800

    dropbox updates

--- code/clean_data.R
+++ code/clean_data.R
@@ -13,57 +13,57 @@ library(dplyr)
 # problems found by grep-ing 'PROF', 'GSHIP', etc. in non_acad
 # problems usually from spaces and dashes
 
-# file 'academic-titles.csv' generated from pdf 'acad-title-codes.pdf'
+# file 'academic_titles.csv' generated from pdf 'acad-title-codes.pdf'
 # csv file generated manually via Excel generating function of Adobe Acrobat
 # from Excel, this was saved as a .csv file
 # manually done because no available dataset not in pdf format;
 # only way to get data in .csv format
 
-if (!file.exists('../data/academic-titles.csv'))
+if (!file.exists('../data/academic_titles.csv'))
 {
-  titles <- read.csv('../rawdata/academic-titles.csv',
+  titles <- read.csv('../rawdata/academic_titles.csv',
                      stringsAsFactors = FALSE)
   titles <- titles[1:511, c(1, 4)]
-  
-  
+
+
   titles$Title[grep('PROF OF CLIN- FY', titles$Title)] <- 'PROF OF CLIN-FY'
-  titles$Title[grep("RES PROF-MILLER INST -AY", titles$Title)] <- 
-    "RES PROF-MILLER INST-AY" 
+  titles$Title[grep("RES PROF-MILLER INST -AY", titles$Title)] <-
+    "RES PROF-MILLER INST-AY"
   titles$Title[grep("PROF EMERITUS \\(WOS\\)", titles$Title)] <-
-    "PROF EMERITUS(WOS)" 
+    "PROF EMERITUS(WOS)"
   titles$Title[grep("LECT SOE-EMERITUS \\(WOS\\)", titles$Title)] <-
     "LECT SOE-EMERITUS(WOS)"
-  
-  titles$Title[grep("ASSOC IN            - AY-1/9-GSHIP" , 
+
+  titles$Title[grep("ASSOC IN            - AY-1/9-GSHIP" ,
                     titles$Title)] <- "ASSOC IN __ -AY-1/9-GSHIP"
-  
-  titles$Title[grep("ASSOC IN            -AY-1/9-NON-GSHIP", 
+
+  titles$Title[grep("ASSOC IN            -AY-1/9-NON-GSHIP",
                     titles$Title)] <- "ASSOC IN__-AY- 1/9 -NON-GSHIP"
-  
-  titles$Title[grep("ASSOC IN            -AY-GSHIP" , 
-                    titles$Title)] <- "ASSOC IN ____-AY-GSHIP" 
-  
-  titles$Title[grep("READER - GSHIP", 
-                    titles$Title)] <- "READER-GSHIP" 
-  
-  titles$Title[grep("REMD TUT I-NON-GSHIP/NON REP", 
+
+  titles$Title[grep("ASSOC IN            -AY-GSHIP" ,
+                    titles$Title)] <- "ASSOC IN ____-AY-GSHIP"
+
+  titles$Title[grep("READER - GSHIP",
+                    titles$Title)] <- "READER-GSHIP"
+
+  titles$Title[grep("REMD TUT I-NON-GSHIP/NON REP",
                     titles$Title)] <- "REMD TUT I-NON GSHIP/NON REP"
-  
-  titles$Title[grep("REMD TUT I-NON-GSHIP", 
+
+  titles$Title[grep("REMD TUT I-NON-GSHIP",
                     titles$Title)] <- "REMD TUT I-NON GSHIP"
-  
-  titles$Title[grep("READER - NON GSHIP", 
+
+  titles$Title[grep("READER - NON GSHIP",
                     titles$Title)] <- "READER-NON GSHIP"
-  
+
   titles$Title[grep("TUT--NON STDNT/NON REP",
                     titles$Title)] <- "TUT-NON STDNT/NON REP"
-  
-  titles$Title[grep("READER - NON STDNT", 
+
+  titles$Title[grep("READER - NON STDNT",
                     titles$Title)] <- "READER-NON STDNT"
-  
+
   # write interim. Some text problems from the pdf->txt process
   # still have to be fixed by hand.
-  write.csv(titles, '../data/academic-titles.csv')
+  write.csv(titles, '../data/academic_titles.csv')
 }
 
 # everything is wrapped in ifs so the files can be sourced without wasting time
@@ -74,7 +74,7 @@ if (!file.exists('../data/academic-titles.csv'))
 
 if (!exists('titles'))
 {
-  titles <- read.csv('../data/academic-titles.csv', strip.white = T,
+  titles <- read.csv('../data/academic_titles.csv', strip.white = T,
                      stringsAsFactors = F)
 }
 
@@ -84,55 +84,55 @@ uctca_colnames <- c("Name", "Title", "Base", "Overtime", "Other", "Benefits",
 # restricting total pay to > $1000, this removes a lot of unnamed/useless rows.
 if (!file.exists('../data/uc2011.csv') || !exists('uc2011'))
 {
-  uc2011 <- read_csv("../rawdata/university-of-california-2011.csv", 
+  uc2011 <- read_csv("../rawdata/university_of_california_2011.csv",
                      col_names = uctca_colnames,
                      skip = 1) %>% filter(Total > 1000)
   # this is not too useful because the 2011 titles are different from the
   # 2012:2014 titles.
   uc2011 <- left_join(uc2011, titles,
                       by = 'Title') %>% mutate(Academic = !is.na(Category))
-  
+
   write.csv(uc2011, '../data/uc2011.csv')
 }
 
 if (!file.exists('../data/uc2012.csv') || !exists('uc2012'))
 {
-  uc2012 <- read_csv("../rawdata/university-of-california-2012.csv", 
+  uc2012 <- read_csv("../rawdata/university_of_california_2012.csv",
                      col_names = uctca_colnames,
                      skip = 1) %>% filter(Total > 1000)
   uc2012 <- left_join(uc2012, titles,
                       by = 'Title') %>% mutate(Academic = !is.na(Category))
-  
+
   write.csv(uc2012, '../data/uc2012.csv')
 }
 
 if (!file.exists('../data/uc2013.csv') || !exists('uc2013'))
 {
-  uc2013 <- read_csv("../rawdata/university-of-california-2013.csv", 
+  uc2013 <- read_csv("../rawdata/university_of_california_2013.csv",
                      col_names = uctca_colnames,
                      skip = 1) %>% filter(Total > 1000)
   # 2013 and 2014 returned problems with read.csv();
   # so we terminate them with prejudice
   attributes(uc2013)$problems <- NULL
-  uc2013 <- left_join(uc2013, titles, 
+  uc2013 <- left_join(uc2013, titles,
                       by = 'Title') %>% mutate(Academic = !is.na(Category))
-  
+
   write.csv(uc2013, '../data/uc2013.csv')
 }
 
 if (!file.exists('../data/uc2014.csv') || !exists('uc2014'))
 {
   # for some reason this dataset has an empty column, so we delete it.
-  # Sometimes however, it does not. 
+  # Sometimes however, it does not.
   # Because the file is so large, we are having trouble figuring out
   # where this originated.
-  uc2014 <- read_csv("../rawdata/university-of-california-2014.csv", 
+  uc2014 <- read_csv("../rawdata/university_of_california_2014.csv",
                      col_names = uctca_colnames,
                      skip = 1) %>% filter(Total > 1000)
   attributes(uc2014)$problems <- NULL
-  uc2014 <- left_join(uc2014, titles, 
+  uc2014 <- left_join(uc2014, titles,
                       by = 'Title') %>% mutate(Academic = !is.na(Category))
-  
+
   write.csv(uc2014, '../data/uc2014.csv')
 }
 
--- skeleton.R
+++ skeleton.R
@@ -23,23 +23,23 @@ dir.create('images')
 # four years worth of financial data from Transparent California
 download.file(paste0('http://transparentcalifornia.com/export/university-',
                      'of-california-2011.csv'),
-              'rawdata/university-of-california-2011.csv')
+              'rawdata/university_of_california_2011.csv')
 download.file(paste0('http://transparentcalifornia.com/export/university-',
                      'of-california-2012.csv'),
-              'rawdata/university-of-california-2012.csv')
+              'rawdata/university_of_california_2012.csv')
 download.file(paste0('http://transparentcalifornia.com/export/university-',
                      'of-california-2013.csv'),
-              'rawdata/university-of-california-2013.csv')
+              'rawdata/university_of_california_2013.csv')
 download.file(paste0('http://transparentcalifornia.com/export/university-',
                      'of-california-2014.csv'),
-              'rawdata/university-of-california-2014.csv')
-  
+              'rawdata/university_of_california_2014.csv')
+
 
 # Resources
 # the original titles PDF
 download.file(paste0('http://www.ucop.edu/academic-personnel-programs/_files/',
               'acad-title-codes/academic-ttles-sorted-title-name.pdf'),
-              'resources/acad-title-codes.pdf')
+              'resources/acad_title_codes.pdf')
 
 # UC student enrollment data
 # below line shows original source but it will return an error as there is an
@@ -51,7 +51,7 @@ download.file(paste0('http://www.ucop.edu/academic-personnel-programs/_files/',
 
 # creating data dictionary in rawdata subdirectory
 
-uc2014.meta <- read.csv('rawdata/university-of-california-2014.csv')
+uc2014.meta <- read.csv('rawdata/university_of_california_2014.csv')
 
 var.names <- colnames(uc2014.meta)
 
@@ -72,13 +72,13 @@ var.storage <- sapply(uc2014.meta, class)
 var.units <- c(NA, NA, 'USD', 'USD', 'USD', 'USD', 'USD', 'USD', 'Date',
                NA, NA)
 
-meta = data.frame(Variable = as.character(var.names), 
+meta = data.frame(Variable = as.character(var.names),
                   Definition = as.character(var.def),
-                  Storage = as.character(var.storage), 
-                  Units = as.character(var.units), 
+                  Storage = as.character(var.storage),
+                  Units = as.character(var.units),
                   row.names = NULL)
 
-write.csv(meta, 'rawdata/meta-data.csv', quote = F, row.names = F)
-                  
-                  
-                  
+write.csv(meta, 'rawdata/meta_data.csv', quote = F, row.names = F)
+
+
+
